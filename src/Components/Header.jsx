de40a1608006c16a21193c716709894201de6a4e
TheRock3T <mr_jaf@mail.ru>
2021-04-22 23:05:24 +0300

    First commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.carlogos.org/car-logos/xpeng-logo.png' />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://storge.pic2.me/cm/3200x1800/652/5845d44193214.jpg'></img>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
