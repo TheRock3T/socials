23db1cfd29ea96461691b70fdb880a5cee895488
TheRock3T <mr_jaf@mail.ru>
2021-04-23 20:32:51 +0300

    added dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import dial from "./Dialogs.module.css"
 
-const Dialogs = () => {
+const Dialogs = (props) => {
     return (
-        <div className={dial.content}>
-            Dialogs
+        <div className={dial.dialogs}>
+            <div className={dial.dialogsItems}>
+                <div className={dial.dialog + ' ' + dial.active}>
+                    Dmitry
+                </div>
+                <div className={dial.dialog}>
+                    Alex
+                </div>
+                <div className={dial.dialog}>
+                    Arina
+                </div>
+                <div className={dial.dialog}>
+                    Galina
+                </div>
+            </div>
+            <div className={dial.messages}>
+                <div className={dial.message}>Hello!</div>
+                <div className={dial.message}>Who u?</div>
+                <div className={dial.message}>I like React!</div>
+            </div>
         </div>
     )
 }
